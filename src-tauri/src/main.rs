37d19ffb5799109b41539b76e50bc9c3dbe546b2
PrivateMe <65495333+privatedev11@users.noreply.github.com>
2023-01-07 13:41:54 +0000

    Edited many files, see full changelog for details

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -14,4 +14,5 @@ fn main() {
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+        
 }
